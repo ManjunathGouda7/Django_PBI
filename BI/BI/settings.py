3930e3d8984964bb7a340470ad9ac637e985a194
Manjunath Gouda <108174010+ManjunathGouda7@users.noreply.github.com>
2026-08-11 15:05:12 +0530

    perf: Add GZip API response compression and DocumentFragment DOM batching

--- BI/BI/settings.py
+++ BI/BI/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'BI.urls'
 
